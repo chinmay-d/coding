8f86634dd1b3797bfc2ba487a1de8f0bc0d852f5
chinmay <bhagyesh.deshmukh29@gmail.com>
2021-12-15 11:48:34 +0530

    add strings

--- leetcode/415.add-strings.cpp
+++ leetcode/415.add-strings.cpp
@@ -8,7 +8,33 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        
+        int n1=num1.length()-1;
+        int n2=num2.length()-1;
+        string ans;
+        if(n1>n2)
+            ans=num1;
+        else
+            ans=num2;
+        int n=ans.length()-1;
+        int sum=0;
+        while(n1>=0 || n2>=0){
+            if(n1>=0){
+                sum+=num1[n1]-'0'; //subtracting '0' from the string converts the digit to an int                
+                n1--;
+            }
+            if(n2>=0)
+            {
+                sum+=num2[n2]-'0';
+                n2--;
+            }
+            ans[n]=sum%10+'0';
+            sum=sum/10;
+            n--;
+        }
+        if(sum){
+            ans.insert(ans.begin(),'0'+sum); // adding '0' in front converts the digit from int to string
+        }
+        return ans;
     }
 };
 // @lc code=end
